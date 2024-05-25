1f2b31b2123a4b44a4dfb46f2051748ac2726bcb
doaa04 <doaarigui0@gmail.com>
2024-05-25 21:31:09 +0100

    game levels

--- test0/Assets/scripts/GameManager.cs
+++ test0/Assets/scripts/GameManager.cs
@@ -5,6 +5,10 @@ public class GameManager : MonoBehaviour {
 
     bool gameHasEnded = false;
     public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+    public void CompleteLevel () {
+        completeLevelUI.SetActive(true);
+    }
     public void EndGame () {
         if (gameHasEnded == false) {
             gameHasEnded = true;
